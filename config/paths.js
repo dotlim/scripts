8bf25d9a473ba39d5954d77a602790641c35b416
kq <oxyzhg@163.com>
2020-03-31 15:47:50 +0800

    add paths config

---
+++ config/paths.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require(fs);
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+  // dotenv
+  dotenv: resolveApp('.env'),
+  // root
+  appRoot: appDirectory,
+  // project dir
+  appPath: resolveApp('.'),
+  // output dir
+  appBuild: resolveApp('build'),
+  // public dir
+  appPublic: resolveApp('public'),
+  // node_modules dir
+  appNodeModules: resolveApp('node_modules'),
+  // entey html template
+  appHtml: resolveApp('public/index.html'),
+  // package.json
+  appPackageJson: resolveApp('package.json'),
+  // src dir
+  appSrc: resolveApp('src'),
+  // entry files
+  appIndexJs: resolveApp('src/index.js')
+};
