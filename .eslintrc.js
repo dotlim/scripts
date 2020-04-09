943967ca8f675ecd5da43a6448444d9aa68ac57a
kq <oxyzhg@163.com>
2020-04-09 21:47:12 +0800

    chore(config): add base config files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+    browser: true
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    'no-console': 0,
+    'no-unused-vars': 0
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  }
+};
