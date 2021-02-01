062ae01812d3942a3ee87b6e4238f37ee6c20693
Kq <oxyzhg@163.com>
2021-02-01 11:43:16 +0800

    refactor repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
-const _ = require('lodash');
-
-const OFF = 'off';
-const WARN = 'warn';
-const ERROR = 'error';
-
-module.exports = _.merge(require('@dotlim/eslint-config').eslint, {
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [require.resolve('@dotlim/fabric/lib/eslint')],
   rules: {
-    'no-cond-assign': WARN,
-    'no-console': OFF,
-    'no-param-reassign': OFF,
-    'no-plusplus': WARN,
-    'no-unused-vars': WARN,
-    'no-restricted-syntax': OFF,
-    'no-trailing-spaces': ERROR,
-    'no-underscore-dangle': OFF,
-    'prefer-const': OFF
-  }
-});
+    // eslint
+    'no-cond-assign': 'warn',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'warn',
+    'no-unused-vars': 'warn',
+    'no-restricted-syntax': 'off',
+    'no-trailing-spaces': 'error',
+    'no-underscore-dangle': 'off',
+    'prefer-const': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ...require('@dotlim/fabric/lib/prettier'),
+  // custom config
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: true,
+};
